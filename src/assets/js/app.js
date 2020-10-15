d161004a1977dfac22fe09e291fcf61fbc9baa0a
Valikwp <valikpbua@gmail.com>
2020-10-15 11:20:26 +0300

    Updates

--- src/assets/js/app.js
+++
@@ -1,5 +0,0 @@
-global.$ = global.jQuery = require('jquery');
-
-(function () {
-  require('./pages/script').default();
-}());
--- src/assets/js/pages/script.js
+++
@@ -1,9 +0,0 @@
-import Slick from 'slick-carousel';
-import balanceText from 'balance-text';
-import cookie from 'jquery.cookie';
-import svg4everybody from 'svg4everybody';
-
-
-export default function () {
-    svg4everybody({});
-}
