7d48e3d04af91d9f7623a394672239adf874d46b
admin <admin@MacBook-Air-admin.local>
2020-10-17 14:24:43 +0300

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -14757,7 +14757,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable: \"$back\".\n        on line 7 of src/assets/styles/settings/_buttons.scss\n        from line 9 of src/assets/styles/settings/_settings.scss\n        from line 1 of /Users/admin/Downloads/medicinform/src/assets/styles/style.scss\n>>   background-color: $back;\n\n   --------------------^\n\n    at /Users/admin/Downloads/medicinform/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/admin/Downloads/medicinform/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/admin/Downloads/medicinform/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/admin/Downloads/medicinform/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/Users/admin/Downloads/medicinform/node_modules/sass-loader/dist/index.js:73:7)\n    at Object.done [as callback] (/Users/admin/Downloads/medicinform/node_modules/neo-async/async.js:8069:18)\n    at options.error (/Users/admin/Downloads/medicinform/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
